3089d40c7a26882cb4de5d18433c05ffc9aed57b
tringuyenle <102273673+tringuyenle@users.noreply.github.com>
2023-10-11 12:50:15 +0700

    fix env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+require("dotenv").config();
 
 @Module({
-  imports: [ MongooseModule.forRoot(process.env.DB_URL), 
-    AuthModule, UserModule],
+  imports: [MongooseModule.forRoot(process.env.DB_URL), AuthModule , UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
