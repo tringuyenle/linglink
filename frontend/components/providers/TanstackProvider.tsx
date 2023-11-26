ed183acd7fccffe5efaa57f6c649126303a89cb3
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2023-11-26 14:32:01 +0700

    feat(login, signin): add API, store userInfor and access

--- frontend/components/providers/TanstackProvider.tsx
+++ frontend/components/providers/TanstackProvider.tsx
@@ -1,14 +1,19 @@
 "use client"
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { useState } from "react"
+import React from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 const TanstackProvider = ({ children }: { children: React.ReactNode }) => {
-    const [queryClient] = useState(() => new QueryClient())
+    // const [queryClient] = useState(() => new QueryClient())
+    const [client] = React.useState(new QueryClient());
     return (
-        <QueryClientProvider client={queryClient}>
-            {children}
+        <QueryClientProvider client={client}>
+            <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
         </QueryClientProvider>
     )
+    // < QueryClientProvider client = { queryClient } >
+    //     { children }
+    // </QueryClientProvider >
 }
 
 export default TanstackProvider
