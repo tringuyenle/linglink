7ab8f481601461db64a48e9b96579c9da90b3882
tringuyenle <102273673+tringuyenle@users.noreply.github.com>
2024-04-01 19:41:53 +0700

    feat: connect socket io to nextjs version 1

--- frontend/app/[locale]/(homepage)/layout.tsx
+++ frontend/app/[locale]/(homepage)/layout.tsx
@@ -4,6 +4,7 @@ import Dictionary from "@/components/dictionary"
 import FlashCardCreate from "@/components/flashcardcreate"
 import Header from "@/components/header";
 import ChatWithGemini from "@/components/geminiAI";
+import ChatWithAnother from "@/components/chat-realtime";
 
 export default function HomeLayout({
   children,
@@ -25,6 +26,7 @@ export default function HomeLayout({
           <div className="scrollbar overflow-auto flex flex-col gap-4 max-h-[80vh]">
             <FlashCardCreate />
             <ChatWithGemini />
+            <ChatWithAnother />
             <Dictionary />
           </div>
         </div>
