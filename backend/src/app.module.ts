179f8de71270b6e3547d71c4fc78658075f9a0f0
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2024-04-09 15:38:03 +0700

    feat: add calendar module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,21 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { AuthModule } from './auth/auth.module'
-import { UserModule } from './user/user.module'
-import { MongooseModule } from '@nestjs/mongoose'
-import { PostsModule } from './posts/posts.module'
-import { TopicsModule } from './topics/topics.module'
-import { TagsModule } from './tags/tags.module'
-import { QuestionsModule } from './questions/questions.module'
-import { FlashcardListModule } from './flashcard-list/flashcard-list.module'
-import { FlashcardModule } from './flashcards/flashcards.module'
-import { CommentsModule } from './comments/comments.module'
-import { ReactionsModule } from './reactions/reactions.module'
-import { CourseModule } from './courses/courses.module'
-import { ChatsModule } from './chat/chats.module'
-require('dotenv').config()
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostsModule } from './posts/posts.module';
+import { TopicsModule } from './topics/topics.module';
+import { TagsModule } from './tags/tags.module';
+import { QuestionsModule } from './questions/questions.module';
+import { FlashcardListModule } from './flashcard-list/flashcard-list.module';
+import { FlashcardModule } from './flashcards/flashcards.module';
+import { CommentsModule } from './comments/comments.module';
+import { ReactionsModule } from './reactions/reactions.module';
+import { CourseModule } from './courses/courses.module';
+import { ChatsModule } from './chat/chats.module';
+import { CalendarModule } from './calendar/calendar.module';
+require('dotenv').config();
 
 @Module({
   imports: [
@@ -30,9 +31,10 @@ require('dotenv').config()
     CommentsModule,
     ReactionsModule,
     CourseModule,
-    ChatsModule
+    ChatsModule,
+    CalendarModule,
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {}
